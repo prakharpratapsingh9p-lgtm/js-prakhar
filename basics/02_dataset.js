1f8ea05e78c25fc368bd0ff12557847b7fababf2
Prakhar77 <prakharpratapsingh9p@gmail.com>
2026-03-13 13:01:38 +0000

    datatype

---
+++ basics/02_dataset.js
@@ -0,0 +1,11 @@
+"use strict"; // treat all JS code as newer  version
+// alert(3+3)  //we are using nodejs ,not brower
+
+// code readablitiy high
+
+let Na,e ="Rex77r"
+let age = 19
+let isLoggedIn = false
+
+//object
+console.log(typeof "praKHAR")
---
+++ basics/varcons.js
@@ -0,0 +1,13 @@
+const accountId = 14453
+let accountEmail = "anything@123"
+var accountPassward ="1234"
+accountCity = "Auraiya"
+let accountState;
+
+// accountId =2
+accountEmail ="prak@12"
+accountPassward ="3456"
+accountCity ="Nagpur"
+
+console.log(accountState);
+console.table([accountEmail , accountPassward,accountCity])
