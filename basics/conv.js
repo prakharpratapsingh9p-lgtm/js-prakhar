d87dd62b1f2e1c2e1a02bd58144871a005155a32
Prakhar77 <prakharpratapsingh9p@gmail.com>
2026-03-14 14:30:59 +0000

    conversion datatype

---
+++ basics/conv.js
@@ -0,0 +1,31 @@
+/*let score = true
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log( valueInNumber);
+
+console.log(typeof NaN);
+
+// "33abd" =>NaN
+
+*/
+
+let isLoggedIn = "k"
+
+let boolIsLoggedIn = Boolean(isLoggedIn)
+
+console.log ( typeof boolIsLoggedIn);
+
+
+
+// 1 => true
+// 0 => false
+//empty =>false
+//filled varaible => true
+
+let sNumber = 88
+let stringNumber = String(sNumber)
+console.log( typeof stringNumber);
