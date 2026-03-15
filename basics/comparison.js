09111c384cd1355de7a157fd84debf63dc44a70b
Prakhar77 <prakharpratapsingh9p@gmail.com>
2026-03-15 21:31:44 +0000

    comparison

---
+++ basics/comparison.js
@@ -0,0 +1,20 @@
+// console.log(4>2);
+// console.log(6 ==1)
+
+ console.log( "2" == 2);
+// console.log("08" > 2);
+
+// console.log(null ==0 );
+// console.log(null >0 );
+// console.log(null <0 );
+// console.log(null >=0 );
+// console.log(null <=0 );
+
+
+// console.log(undefined ==0 )
+// console.log(undefined >=0 )
+// console.log(undefined <=0 )
+
+console.log("2" === 2 )
+
+
