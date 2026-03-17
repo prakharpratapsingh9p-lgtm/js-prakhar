595553af853d83d71655bd6902ffc86315c8b358
Prakhar77 <prakharpratapsingh9p@gmail.com>
2026-03-17 14:00:37 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,56 @@
+/*
+primitive (call by value)
+ 7 types
+  1) string
+  2)number
+  3) Boolean
+  4)null
+  5) undefined
+  6) symbol - uniquess
+  7) big int
+
+
+
+
+  Non primitive / Reference type
+
+  1) arrays
+  2) objects
+  3) Functions
+
+
+
+const isLoggedIn = null
+let p ;
+console.log(typeof p);
+
+const id = Symbol('123')
+
+console.log(id);
+console.log(i1d)
+
+
+
+
+const names= ["prakhar" , " parsoon",  "Annaya"
+]
+
+console.log(names)
+
+
+let myPy= {
+    q : "prakhar",
+    age : 22,
+}
+
+console.log(myPy)
+
+
+*/
+
+ const m = function(){
+    console.log( "prakhar");
+    
+ }
+const i1d =Symbol(123)
+console.log(typeof i1d);
