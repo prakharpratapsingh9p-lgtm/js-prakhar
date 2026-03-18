7a1b64a7cfa306441a8c22fa6afd1046b11a114e
Prakhar77 <prakharpratapsingh9p@gmail.com>
2026-03-18 15:01:09 +0000

    string

---
+++ basics/string.js
@@ -0,0 +1,46 @@
+const name = "Prakhar"
+const rollNo = 77
+
+// console.log(name + rollNo +" you are the best");
+
+//console.log(` hello my name is ${name}  and my rollNo is ${rollNo} You are the best `);
+
+const gameName = new String('Prakhar')
+/*
+console.log(gameName[0]);
+
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+
+*/
+
+console.log(gameName.charAt(3)); 
+
+console.log(gameName.indexOf('r'));
+
+const newString =gameName.substring(0,4)
+//console.log(newString);
+
+
+const anotherString = gameName.slice(-7, 6)
+//console.log(anotherString)
+
+
+const newString1 = "Prakhar pratap singh"
+console.log(newString1);
+console.log(newString1.trim());
+
+
+const url ="https:/Prakhar.com/prakhar#77"
+
+
+console.log(url.replace('#', '-'));
+
+console.log(newString1.split(' '));
+
+console.log(newString1.split(''));
+
+
+
