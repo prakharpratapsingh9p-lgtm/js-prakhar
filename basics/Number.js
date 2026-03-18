fe145a0872c24ff631251f3301f5ea042fe70500
Prakhar77 <prakharpratapsingh9p@gmail.com>
2026-03-18 17:24:01 +0000

    numbers

---
+++ basics/Number.js
@@ -0,0 +1,15 @@
+/*const score = 900
+console.log(score)
+
+const balance  = new Number(200.454545)
+console.log(balance)
+
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const otherNumber= 435.438898
+console.log(otherNumber.toPrecision(4));
+*/
+const hundreds = 10000000
+console.log(hundreds.toLocaleString('en-IN'));
