b92755e930c28059627240bbc5caca8a4553832e
Prakhar77 <prakharpratapsingh9p@gmail.com>
2026-03-18 17:50:09 +0000

    mathhalf

--- basics/Number.js
+++ basics/Number.js
@@ -10,6 +10,23 @@ console.log(balance.toFixed(2));
 
 const otherNumber= 435.438898
 console.log(otherNumber.toPrecision(4));
-*/
+
 const hundreds = 10000000
 console.log(hundreds.toLocaleString('en-IN'));
+
+
+//++++++++++ MATHS ++++
+
+
+console.log(Math);
+console.log(Math.abs(-9));
+console.log(Math.round(34.27));
+console.log(Math.ceil(45.1));
+console.log(Math.floor(34.9));
+console.log(Math.min(89,7,1919));
+console.log(Math.max(89,7,1919));
+
+*/
+console.log(Math.random()*10 +1);
+
+console.log(Math.floor(Math.random()*10 +1));
